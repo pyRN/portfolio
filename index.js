2947a241c8ce2ae56d25ad908d8dbdd3a2eb0d8f
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-15 11:45:03 -0500

    Added more section to about me, tied in functionality and styling.  Needs better narrative!

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 //Used to transform hamburger menu and open/close when clicked on small screens
-function fnOpenMenu(hamburgerMenuElement) {
-  hamburgerMenuElement.classList.toggle("open");
+function fnOpenMenu(oHamburgerMenuElement) {
+  oHamburgerMenuElement.classList.toggle("open");
 }
 
-//Used to scroll to contact me section
-function fnContactMe() {
-  console.log("here");
-  document.getElementById("contact-section").scrollIntoView();
+function fnShowMore() {
+  var oEllipsis = document.getElementById("show-more");
+  var oMoreText = document.getElementById("more-text");
+  var oShowLess = document.getElementById("show-less");
+
+  if (oEllipsis.style.display === "none") {
+    oEllipsis.style.display = "inline";
+    oShowLess.style.display = "none";
+    oMoreText.style.display = "none";
+  } else {
+    oEllipsis.style.display = "none";
+    oShowLess.style.display = "inline";
+    oMoreText.style.display = "inline";
+  }
 }
